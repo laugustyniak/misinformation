c5ae0aae478da2f2c2ad23cdb34b8313593d8e31
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2020-07-09 16:37:58 +0200

    categories names mapping

---
+++ political_advertising/categories.py
@@ -0,0 +1,11 @@
+POLITICAL_ADVERTISING_CATEGORIES_EN_TO_PL = {
+    'defense and security': 'Obronność i bezpieczeństwo',
+    'education': 'Edukacja',
+    'foreign policy': 'Polityka zagraniczna',
+    'healhcare': 'Służba zdrowia',
+    'immigration': 'Imigracja',
+    'infrastructure and enviroment': 'Infrastruktura i środowisko',
+    'political and legal system': 'System polityczny i prawny',
+    'society': 'Społeczeństwo',
+    'welfare': 'Dobrobyt'
+}
