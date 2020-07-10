9eacd0e1ebcc0ef32d2093e8015253d4ca9d1a34
Łukasz Augustyniak <luk.augustyniak@gmail.com>
2020-07-10 02:45:38 +0200

    google tag manager code

--- political_advertising_dashboard.py
+++ political_advertising_dashboard.py
@@ -241,14 +241,16 @@ st.write(
 
 # GOOGLE ANALYTICS
 st.markdown("""
-<!-- Global site tag (gtag.js) - Google Analytics -->
-<script async src="https://www.googletagmanager.com/gtag/js?id=UA-172057973-1"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments);}
-  gtag('js', new Date());
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-TXZW3XX');</script>
+<!-- End Google Tag Manager -->
 
-  gtag('config', 'UA-172057973-1');
-</script>
-
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TXZW3XX"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
 """, unsafe_allow_html=True)
